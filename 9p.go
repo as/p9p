7cd73264203776fa8492821b565397e2676ffd1c
as <as.utf8@gmail.com>
2018-03-04 19:36:36 -0800

    p9p: refactor

--- 9p.go
+++ 9p.go
@@ -1,28 +1,164 @@
 package p9p
 
+import (
+	"bufio"
+	"errors"
+	"io"
+	"log"
+	"net"
+)
+
 type Kind byte
 
 const (
-	KindTversion, KindRversion Kind = iota + 100, iota + 101
-	KindTauth, KindRauth
-	KindTerror, KindRerror
-	KindTflush, KindRflush
-	KindTwalk, KindRwalk
-	KindTopen, KindRopen
-	KindTcreate, KindRcreate
-	KindTread, KindRread
-	KindTwrite, KindRwrite
-	KindTclunk, KindRclunk
-	KindTremove, KindRremove
-	KindTstat, KindRstat
-	KindTwstat, KindRwstat
+	KTversion, KRversion byte = iota + 100, iota + 101
+	KTauth, KRauth
+	KTerror, KRerror
+	KTflush, KRflush
+	KTwalk, KRwalk
+	KTopen, KRopen
+	KTcreate, KRcreate
+	KTread, KRread
+	KTwrite, KRwrite
+	KTclunk, KRclunk
+	KTremove, KRremove
+	KTstat, KRstat
+	KTwstat, KRwstat
 )
+const NOTAG = ^uint16(0)
+
+func str(in string) s {
+	return s{
+		n:    uint16(len(in)),
+		data: []byte(in),
+	}
+}
 
 //wire9 qid data[13]
 //wire9 s n[2] data[n]
 //wire9 Hdr size[4] msg[1]
+
 //wire9 Tversion size[4] msg[1] tag[2] msize[4] version[,s]
 //wire9 Rversion size[4] msg[1] tag[2] msize[4] version[,s]
+
+const (
+	Version = "9p2000"
+	MaxMsg  = 65536
+)
+
+func NewBio(conn net.Conn) *Bio {
+	return &Bio{
+		ReadWriter: bufio.NewReadWriter(
+			bufio.NewReaderSize(conn, MaxMsg),
+			bufio.NewWriterSize(conn, MaxMsg),
+		),
+	}
+}
+
+var (
+	ErrNoConn = errors.New("no connection")
+	ErrBadVersion = errors.New("bad version")
+)
+
+type State byte
+
+const (
+	StClosed State = iota
+	StSyncer
+	StSyncee
+	StEstablished
+	StError
+)
+
+type Bio struct {
+	*bufio.ReadWriter
+	rwc     io.ReadWriteCloser
+	err     error
+	version string
+	state   State
+}
+
+func (b *Bio) Version() (string, error) {
+	if b.state != StEstablished {
+		return "", ErrNoConn
+	}
+	if b.version == ""{
+		return "", ErrBadVersion
+	}
+	return b.version, nil
+}
+
+func Accept(fd net.Listener) (*Bio, error) {
+	conn, err := fd.Accept()
+	if err != nil {
+		return nil, err
+	}
+	bio := NewBio(conn)
+
+	tv := Tversion{}
+
+	if err = tv.ReadBinary(bio); err != nil {
+		panic(err)
+	}
+
+	log.Printf("accept: got %#v\n", tv)
+
+	rv := Rversion{
+		size:    uint32(4 + 1 + 2 + 4 + len(Version)),
+		msg:     KTversion,
+		tag:     NOTAG,
+		msize:   MaxMsg,
+		version: str(Version),
+	}
+	if err := rv.WriteBinary(bio); err != nil {
+		log.Printf("open: err: %s\n", err)
+	}
+
+	bio.state = StEstablished
+	bio.version = string(rv.version.data)
+	
+	return bio, bio.Flush()
+}
+
+func Dial(netw string, addr string) (*Bio, error) {
+	conn, err := net.Dial(netw, addr)
+	if err != nil {
+		return nil, err
+	}
+
+	bio, err := Open(conn)
+	if err != nil {
+		conn.Close()
+	}
+
+	return bio, err
+}
+
+func Open(conn net.Conn) (*Bio, error) {
+	bio := NewBio(conn)
+
+	tv := Tversion{
+		size:    uint32(4 + 1 + 2 + 4 + len(Version)),
+		msg:     KTversion,
+		tag:     NOTAG,
+		msize:   MaxMsg,
+		version: str(Version),
+	}
+	log.Printf("open: sending %#v\n", tv)
+	if err := tv.WriteBinary(bio); err != nil {
+		log.Printf("open: err: %s\n", err)
+	}
+	err := bio.Flush()
+	if err != nil{
+		return nil, err
+	}
+	
+	bio.state = StEstablished
+	bio.version = string(tv.version.data)
+
+	return bio, err
+}
+
 //wire9 Tauth size[4] msg[1] tag[2] afid[4] uname[,s] aname[,s]
 //wire9 Rauth size[4] msg[1] tag[2] aqid[13]
 //wire9 Rerror size[4] msg[1] tag[2] ename[,s]
@@ -50,4 +186,3 @@ const (
 //wire9 Rstat size[4] msg[1] tag[2] stat[size-4-1-2]
 //wire9 Twstat size[4] msg[1] tag[2] fid[4] stat[size-4-1-2]
 //wire9 Rwstat size[4] msg[1] tag[2]
-ECHO is on.
