926cce2fa809c4ddfc1df862fe259f8212ae4fce
as <as.utf8@gmail.com>
2018-03-08 01:11:44 -0800

    p9p: make it pass tests

--- msg_test.go
+++ msg_test.go
@@ -4,8 +4,8 @@ import "testing"
 
 func TestString(t *testing.T) {
 	c, s := testConn(t)
-		c.writestring("hello")
-		c.Flush()
+	c.writestring("hello")
+	c.Flush()
 	s.readstring()
 	if s.String() != "hello" {
 		t.Fatalf("have %s want %q err %s", s.String(), "hello", s.err)
