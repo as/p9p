5aa17150c0271cb2c070c16c38ec785224ea4781
as <as.utf8@gmail.com>
2018-03-11 06:41:36 -0700

    p9p: seperate plan9 tests

--- version_test.go
+++ version_test.go
@@ -1,13 +1,9 @@
 package p9p
 
 import (
-	"context"
 	"net"
 	"testing"
-	"time"
 )
-
-const realPlan9 = "1.1.1.1:808"
 
 func testConn(t *testing.T) (client, server *Conn) {
 	t.Helper()
@@ -59,59 +55,6 @@ func TestVersion(t *testing.T) {
 
 	if cv != sv {
 		t.Fatalf("client and server differ: %q vs %q\n", cv, sv)
-	}
-
-}
-
-func ckHasPlan9(t *testing.T) {
-	t.Helper()
-	if realPlan9 == "" {
-		t.Skip("no real plan9 defined")
-	}
-	result := make(chan error)
-	ctx, fn := context.WithTimeout(context.Background(), time.Second)
-	defer fn()
-	go func() {
-		dialer := net.Dialer{}
-		conn, err := dialer.DialContext(ctx, "tcp", realPlan9)
-		result <- err
-		if err == nil {
-			conn.Close()
-		}
-	}()
-
-	select {
-	case <-ctx.Done():
-	case err := <-result:
-		if err == nil {
-			return
-		}
-	}
-
-	t.Skip("plan9 isn't responding here:", realPlan9)
-
-}
-
-// The server should run the equivalent of
-//	aux/listen1 -t -v tcp!*!808 exportfs -r /
-//
-func TestVersionPlan9(t *testing.T) {
-	ckHasPlan9(t)
-	conn, err := Dial("tcp", realPlan9)
-	if err != nil {
-		t.Fatal(err)
-	}
-	for i := 0; i < 2; i++ {
-		max, version, err := conn.Ver()
-		max = max
-		version = version
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
-	err = conn.Attach(1, 2, "/root", "xxxxxxxxx")
-	if err != nil {
-		t.Fatal(err)
 	}
 
 }
