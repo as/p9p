7fff184d5d2bb5e023c23d59c8df7e5a2b5e9ac6
as <as.utf8@gmail.com>
2018-03-11 09:11:36 -0700

    p9p: open and create

---
+++ open.go
@@ -0,0 +1,35 @@
+package p9p
+
+func (c *Conn) Open(fid int, mode byte) (q Qid, iounit uint32, err error){
+	defer func(){logf("Open: %v %s %s", fid, mode, err)}()
+	m := &Msg{src: c}
+	if !m.writeHeader(KTopen) || !m.writebinary(uint32(fid)) || !m.writebinary(mode) {
+		return q, iounit, m.err
+	}
+
+	if !c.schedule(m) {
+		return q, iounit, m.err
+	}
+
+	m.readbinary(&q)
+	m.readbinary(&iounit)
+	return q, iounit, m.err
+}
+
+
+func (c *Conn) Create(fid int, name string, perm uint32, mode byte) (q Qid, iounit uint32, err error){
+	defer func(){logf("Create: %v %s %s", fid, mode, err)}()
+	m := &Msg{src: c}
+	if !m.writeHeader(KTcreate) || !m.writebinary(uint32(fid)) || !m.writestring(name) || !m.writebinary(perm)|| !m.writebinary(mode){
+		return q, iounit, m.err
+	}
+
+	if !c.schedule(m) {
+		return q, iounit, m.err
+	}
+
+	m.readbinary(&q)
+	m.readbinary(&iounit)
+	return q, iounit, m.err
+}
+
