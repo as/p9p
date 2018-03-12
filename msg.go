ff4d04fa73dc32b3944c8951e70f3999719625f1
as <as.utf8@gmail.com>
2018-03-12 06:11:35 -0700

    p9p: organize msg types

--- msg.go
+++ msg.go
@@ -25,8 +25,12 @@ type Msg struct {
 	err error
 }
 
+type Names struct {
+	List []string
+}
+
 func (m *Msg) String() string {
-	return fmt.Sprintf("(%d %s %d) -> %q\n",
+	return fmt.Sprintf("size[%d] kind[%s] tag[%d] -> data[%q]\n",
 		m.Header.Size,
 		m.Header.Kind,
 		m.Header.Tag,
@@ -34,8 +38,90 @@ func (m *Msg) String() string {
 	)
 }
 
-type Names struct {
-	List []string
+func (c *Msg) size() uint32 {
+	return 4 + 1 + 2 + uint32(c.Buffer.Len())
+}
+
+func (c *Msg) write(p []byte) bool {
+	if c.err == nil {
+		_, c.err = c.Buffer.Write(p)
+	}
+	return c.err == nil
+}
+
+func (c *Msg) read(size uint32) bool {
+	if c.err == nil {
+		_, c.err = io.Copy(&(c.Buffer), io.LimitReader(c.src, int64(size)))
+	}
+	return c.err == nil
+}
+
+func (c *Msg) writebinary(v interface{}) bool {
+	if c.err == nil {
+		c.err = binary.Write(&c.Buffer, binary.LittleEndian, v)
+	}
+	return c.err == nil
+}
+
+func (c *Msg) readbinary(v interface{}) bool {
+	if c.err == nil {
+		c.err = binary.Read(c.src, binary.LittleEndian, v)
+	}
+	return c.err == nil
+}
+
+func (c *Msg) writeMsg(kind Kind, p []byte) bool {
+	return c.writeHeader(kind) && c.write(p)
+}
+
+func (c *Msg) readMsg(kind Kind) bool {
+	return c.readHeader() && c.read(c.Header.Size-4-1-2)
+}
+
+func (c *Msg) writeHeader(kind Kind) bool {
+	if c.err == nil {
+		c.Header.Kind = kind
+	}
+	return c.err == nil
+}
+
+func (c *Msg) readHeader() bool {
+	if c.err == nil {
+		c.Buffer.Reset()
+	}
+	return c.readbinary(&c.Header)
+}
+
+func (c *Msg) readbytes(p []byte) bool {
+	if c.err == nil {
+		_, c.err = io.ReadAtLeast(&c.Buffer, p, len(p))
+	}
+	return c.err == nil
+}
+
+func (c *Msg) writestring(s string) bool {
+	return c.writebinary(uint16(len(s))) && c.write([]byte(s))
+}
+
+func (c *Msg) readstring() bool {
+	var n uint16
+	return c.readbinary(&n) && c.read(uint32(n))
+}
+
+func (m *Msg) readQuids(q *[]Qid) bool {
+	var (
+		nn uint16
+		q0 Qid
+	)
+	m.readbinary(&nn)
+	for i := uint16(0); i < nn; i++ {
+		if !m.readbinary(&q0) {
+			break
+		}
+		*q = append(*q, q0)
+	}
+	return m.err == nil
+
 }
 
 func (m *Msg) writeNames(names ...string) bool {
@@ -48,33 +134,12 @@ func (m *Msg) writeNames(names ...string) bool {
 	return m.err == nil
 }
 
-func (m *Msg) readQuids(q *[]Qid) bool {
-	var (
-		nn uint16
-		q0 Qid
-	)
-	if !m.readbinary(&nn) {
-		return false
-	}
-
-	for i := uint16(0); i < nn; i++ {
-		if !m.readbinary(&q0) {
-			break
-		}
-		*q = append(*q, q0)
-	}
-	return m.err == nil
-
-}
-
 func (m *Msg) readNames(nm *Names) bool {
 	var (
 		nn, n uint16
 		b     [65536]byte
 	)
-	if !m.readbinary(&nn) {
-		return false
-	}
+	m.readbinary(&nn)
 	for i := uint16(0); i < nn; i++ {
 		if !m.readbinary(&n) || !m.readbytes(b[:n]) {
 			break
@@ -84,90 +149,8 @@ func (m *Msg) readNames(nm *Names) bool {
 	return m.err == nil
 }
 
-func (c *Msg) writeMsg(kind Kind, p []byte) bool {
-	return c.writeHeader(kind) && c.write(p)
-}
-
-func (c *Msg) writeHeader(kind Kind) bool {
-	if c.err != nil {
-		return false
-	}
-
-	c.Header.Kind = kind
-	logf("c.header=%s\n", kind)
-
-	return true
-}
-
 func (c *Msg) self() bool {
 	c.src = bytes.NewReader(c.Buffer.Bytes())
 	c.Buffer = bytes.Buffer{}
-	return true
-}
-
-func (c *Msg) size() uint32 {
-	return 4 + 1 + 2 + uint32(c.Buffer.Len())
-}
-
-func (c *Msg) readMsg(kind Kind) bool {
-	return c.readHeader() && c.read(c.Header.Size-4-1-2)
-}
-
-func (c *Msg) readHeader() bool {
-	c.Buffer.Reset()
-	if !c.readbinary(&c.Header) {
-		return false
-	}
-
-	return true
-}
-
-func (c *Msg) readbinary(v interface{}) bool {
-	if c.err != nil {
-		return false
-	}
-	c.err = binary.Read(c.src, binary.LittleEndian, v)
 	return c.err == nil
-}
-
-func (c *Msg) readbytes(p []byte) bool {
-	if c.err != nil {
-		return false
-	}
-	_, c.err = io.ReadAtLeast(&c.Buffer, p, len(p))
-	return c.err == nil
-}
-
-func (c *Msg) read(size uint32) bool {
-	if c.err != nil {
-		return false
-	}
-	_, c.err = io.Copy(&(c.Buffer), io.LimitReader(c.src, int64(size)))
-	return c.err == nil
-}
-
-func (c *Msg) readstring() bool {
-	var n uint16
-	return c.readbinary(&n) && c.read(uint32(n))
-}
-
-func (c *Msg) writebinary(v interface{}) bool {
-	if c.err != nil {
-		return false
-	}
-	c.err = binary.Write(&c.Buffer, binary.LittleEndian, v)
-	return c.err == nil
-}
-
-func (c *Msg) write(p []byte) bool {
-	if c.err != nil {
-		return false
-	}
-
-	_, err := c.Buffer.Write(p)
-	return err == nil
-}
-
-func (c *Msg) writestring(s string) bool {
-	return c.writebinary(uint16(len(s))) && c.write([]byte(s))
 }
